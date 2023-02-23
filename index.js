ac6d5247392f38b1abc22ba45e44bd97bfb2733d
vadim l <vadimlozinschii@gmail.com>
2023-02-23 09:13:16 -0800

    manipulated the dom through modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainL = document.getElementById("main")
+mainL.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
